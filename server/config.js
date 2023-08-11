29423de137922e90bcf84ae5c43e2b0e744f0973
JakeLewisMedler <jake@jakemedler.co.uk>
2023-08-11 13:48:00 +0100

    real database

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  dbServerURL: "http://127.0.0.1:5050",
-  // dbServerURL: "http://10.0.0.50/scoreboard.php",
+  // dbServerURL: "http://127.0.0.1:5050",
+  dbServerURL: "http://10.0.0.50/scoreboard.php",
 
   // dbServerURL: "http://munchingsquare.com/adidas/scoreboard.php",
 };
